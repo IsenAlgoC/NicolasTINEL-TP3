5b02f9d757d3cbfd8f5d43f9c37b243edb28f917
Nicolas-Tinel <nicolas.tinel@isen.yncrea.fr>
2020-09-30 12:15:26 +0200

    Update Exo3.c

--- Project3/Exo3/Exo3.c
+++ Project3/Exo3/Exo3.c
@@ -1,105 +1,91 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <locale.h>
-#include <conio.h>
-#include <ctype.h>
-
-int main()
-{
-	setlocale(LC_ALL, "fr-FR");
-	int compteur = 0,
-		a = 0,
-		i = 0,
-		nb_min = 0,
-		nb_max = 0,
-		absence = 0;
-
-
-	float
-		total = 0,
-		note = 0,
-		note_max = 0,
-		note_min = 0,
-		tab[30] = { 0 },
-		moyenne = 0;
-
-
-
-
-	while (compteur < 30)
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#define NBMAXNOTES 30
+void main() {
+	int i = 0;
+	float note[NBMAXNOTES];
+	float note_min = 20;
+	float note_max = 0;
+	char input = 'O';
+	int valide = 0, nb_max = 0, nb_min = 0;
+	int absent = 0;
+	for (int i = 0; i < NBMAXNOTES; i++)
 	{
-		printf("introduisez une note: \n");
-		scanf_s("%f", &note);
-		
-		tab[i] = note;
-		i++;
+		note[i] = -2;
+	}
+	while (i < 30)
+	{
+		printf("Entrer la note n° %d :", i);
+		scanf_s("%f", &note[i]);
+		if ((note[i] < 0.0) || (note[i] > 20.0))
+		{
 
-		/*traitement de la note*/
-		if (note >= 0 && note <= 20) {
-			compteur = compteur + 1;
-
-			if (note >= note_max)
+			printf("Elève absent ? ou voulez-vous arrêter la saisie des notes ? A/O/N ");
+			char ch = '\0';
+			ch = toupper(ch);
+			switch(ch)
 			{
-				if (note == note_max)
+			case'A':
+				note[i] = -1;
+				absent = absent + 1;
+				i = i + 1;
+				break;
+			case'O':
+				i = 30;
+				break;
+			case'N':
+				i = -1;
+				break;
+			}
+		}
+		else
+		{
+			if (note[i] >= note_max)
+			{
+				if (note[i] == note_max)
 					nb_max++;
 				else
 				{
-					note_max = note;
+					note_max = note[i];
 					nb_max = 1;
 				}
 			}
-			if ((note <= note_min) && (note > 0))
+			if ((note[i] <= note_min) && (note[i] > 0))
 			{
-				if (note == note_min)
+				if (note[i] == note_min)
 					nb_min++;
 				else
 				{
-					note_min = note;
+					note_min = note[i];
 					nb_min = 1;
 				}
 			}
-		}
-		//si la note n'est pas valide /
-		if (note < 0.00 || note > 20.00) {
-			printf("\nVous avez tapé une note invalide, l'élève est-il (A)bsent ou voulez vous arrêter la saisie (O)ui (N)on ?");
-			char ch = '\0';
-			ch = (char)_getch();  //permet de poser une question à l'utilisateur
-			ch = toupper(ch);
-			switch (ch)
-			{
-			case 'A':
-				absence += 1;
-				break;
-			case 'O':
-				compteur = 30;
-				break;
-
-			case 'N':
-				break;
-			}
-
+			valide = valide + 1;
+			i = i + 1;
 		}
 	}
-	if (compteur != 0)
+	float quotient = 0;
+	float total = 0.0;
+	float moyenne = 0.0;
+	for (int i = 0; i < NBMAXNOTES; i++)
 	{
-
-		for (i > 0; i < compteur; i++) {
-			printf("\n%.2f", tab[i]);
-			total += tab[i];
+		if ((note[i] >= 0.0) && (note[i] <= 20))
+		{
+			quotient = quotient + 1;
+			total = total + note[i];
 		}
-		moyenne = total / (compteur);
-
-		/*affichage des notes*/
-		printf("\n La note moyenne est de :%.2f", moyenne);
-		printf("\nLa note minimal est :%.2f", note_min);
-		printf("\nIl y %d notes minimales", nb_min);
-		printf("\nLa note maximale est de :%.2f", note_max);
-		printf("\nIl y a %d notes maximales", nb_max);
-		printf("\nL'élève a %d heure(s) d'absence.", absence);
 	}
-	else {
-	printf("Aucune note valide !\n");
+	moyenne = total / quotient;
+	double stepone = 0;
+	double ecarttype = 0;
+	for (int i = 0; i < NBMAXNOTES; i++)
+	{
+		if ((note[i] >= 0.0) && (note[i] <= 20))
+		{
+			stepone = stepone + pow(2, (note[i] - moyenne));
+		}
 	}
-
+	ecarttype = (1 / (quotient - 1)) * stepone;
+	printf("Notes valides : %d, Note min : %f, Note max : %f, Nombre d'absences : %d, Moyenne : %f, Ecarttype : %f", valide, note_min, note_max, absent, moyenne, ecarttype);
 }
-		
